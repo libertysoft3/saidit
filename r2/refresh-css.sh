0baa4571dddc679b4735cacdc7766e87b1cd5d3d
libertysoft3 <libertysoft3@protonmail.com>
2019-10-23 04:05:49 +0000

    minor

--- r2/refresh-css.sh
+++ r2/refresh-css.sh
@@ -3,9 +3,5 @@
 sudo -u reddit make build/mangle-buildstamp
 sudo -u reddit make clean_css
 sudo -u reddit make css
-sudo reddit-flush
-sleep 10
+sleep 5
 sudo service reddit-paster restart
-sudo service reddit-paster2 restart
-sudo service reddit-paster3 restart
-sudo service reddit-paster4 restart
