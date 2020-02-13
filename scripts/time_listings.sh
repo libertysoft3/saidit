ea75c2c06d4b66d9bfd648e9da3f831cfa371598
libertysoft3 <libertysoft3@protonmail.com>
2020-02-13 00:08:31 -0800

    compute_time_listings optimizations

--- scripts/time_listings.sh
+++ scripts/time_listings.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# SAIDIT: run sequentially and sleep for max single server performance
+# SAIDIT: running sequentially for single server performance
+# see also WRITE_PERMACACHE_NPROC, WRITE_PERMACACHE_SLEEP
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
-/bin/bash $DIR/compute_time_listings link year "['hour', 'day', 'week', 'month', 'year']" 2>&1 | /usr/bin/logger -t compute_time_listings_link
-sleep 10
-/bin/bash $DIR/compute_time_listings comment year "['hour', 'day', 'week', 'month', 'year']" 2>&1 | /usr/bin/logger -t compute_time_listings_comment
+
+/bin/bash $DIR/compute_time_listings link year "['year']" 2>&1 | /usr/bin/logger -t compute_time_listings_link
+/bin/bash $DIR/compute_time_listings link month "['month']" 2>&1 | /usr/bin/logger -t compute_time_listings_link
+/bin/bash $DIR/compute_time_listings link week "['week']" 2>&1 | /usr/bin/logger -t compute_time_listings_link
+/bin/bash $DIR/compute_time_listings link day "['day']" 2>&1 | /usr/bin/logger -t compute_time_listings_link
+/bin/bash $DIR/compute_time_listings link hour "['hour']" 2>&1 | /usr/bin/logger -t compute_time_listings_link
+
+/bin/bash $DIR/compute_time_listings comment year "['year']" 2>&1 | /usr/bin/logger -t compute_time_listings_comment
+/bin/bash $DIR/compute_time_listings comment month "['month']" 2>&1 | /usr/bin/logger -t compute_time_listings_comment
+/bin/bash $DIR/compute_time_listings comment week "['week']" 2>&1 | /usr/bin/logger -t compute_time_listings_comment
+/bin/bash $DIR/compute_time_listings comment day "['day']" 2>&1 | /usr/bin/logger -t compute_time_listings_comment
+/bin/bash $DIR/compute_time_listings comment hour "['hour']" 2>&1 | /usr/bin/logger -t compute_time_listings_comment
